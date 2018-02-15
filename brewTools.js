e7bee19cee3b3f213e02d48b0502160987bd2391
Jordan Miller <jordan.miller@bluescopebuildingsna.com>
2018-02-15 14:08:14 -0600

    Added .mp3 alarm to timer 1

--- brewTools.js
+++ brewTools.js
@@ -1,21 +1,50 @@
 console.log("brewTools.js init");
 
-function startTimer()
-{
-    console.log("Timer has started");
-    var secondsToWait = 5;
-    setTimeout(doSomething, secondsToWait * 1000);
+var HopAdditionOneTimer = undefined;
+var HopAdditionOneTicker = undefined;
+var HopOneRemainingMinutes = 0;
+var HopOneRemainingSeconds = 0;
 
-    alert("start button works");
+function startHopOneTimer()
+{
+    console.log("Hop 1 timer started");
+    HopOneRemainingMinutes = 0;
+    HopOneRemainingSeconds = 7;
+    var totalSeconds = HopOneRemainingMinutes * 60 + HopOneRemainingSeconds;
+
+    HopAdditionOneTimer = setTimeout(alarmHopOne, totalSeconds * 1000);
+
+    HopAdditionOneTicker = setInterval(tickHopeOne, 1000);
 }
 
-function resetTimer()
+function tickHopeOne()
 {
-    console.log("Timer has reset");
-    alert("restart button works");
+    console.log(HopOneRemainingSeconds);
+    HopOneRemainingSeconds--;
+    if(HopOneRemainingSeconds < 0)
+    {
+        HopOneRemainingSeconds = 59;
+        HopOneRemainingMinutes--;
+        if (HopOneRemainingMinutes < 0)
+        {
+            clearInterval(HopAdditionOneTicker);
+        }
+    }
 }
 
-function doSomething()
+function resetHopOneTimer()
 {
-    alert("Neat");
+    clearTimeout(HopAdditionOneTimer);
+    clearInterval(HopAdditionOneTicker);
+    console.log("Hop 1 timer has reset");
+}
+
+function alarmHopOne()
+{
+    console.log("Hop 1 timer done");
+
+    var audio = new Audio('./audio/timerAlarm.mp3');
+    audio.play();
+
+    alert("Hop 1 Timer has Finshed!");
 }
