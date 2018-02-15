377f9fe43951e115af1533c876080f20fde11e98
Jordan Miller <jordan.miller@bluescopebuildingsna.com>
2018-02-15 12:23:34 -0600

    Added margin to brew tools card and button functionallity

--- brewTools.js
+++ brewTools.js
@@ -3,6 +3,9 @@ console.log("brewTools.js init");
 function startTimer()
 {
     console.log("Timer has started");
+    var secondsToWait = 5;
+    setTimeout(doSomething, secondsToWait * 1000);
+
     alert("start button works");
 }
 
@@ -10,4 +13,9 @@ function resetTimer()
 {
     console.log("Timer has reset");
     alert("restart button works");
+}
+
+function doSomething()
+{
+    alert("Neat");
 }
