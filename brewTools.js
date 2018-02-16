bae65058adc161a6914129c7019ea55079cbc23e
Jordan Miller <jordan.miller@bluescopebuildingsna.com>
2018-02-16 14:23:25 -0600

    Hop 1 timer fully operational

--- brewTools.js
+++ brewTools.js
@@ -5,29 +5,12 @@ var HopAdditionOneTicker = undefined;
 var HopOneRemainingMinutes = 0;
 var HopOneRemainingSeconds = 0;
 var hop1Label = undefined;
+var audio = new Audio('./audio/timerAlarm.mp3');
 
-function add5Minutes()
+function addSubtractMinutes(mins)
 {
+    HopOneRemainingMinutes = HopOneRemainingMinutes + mins;
     hop1Label.innerText = HopOneRemainingMinutes + " min : " + HopOneRemainingSeconds + " sec";
-    HopOneRemainingMinutes = HopOneRemainingMinutes + 5;
-}
-
-function add1Minute()
-{
-    hop1Label.innerText = HopOneRemainingMinutes + " min : " + HopOneRemainingSeconds + " sec";
-    HopOneRemainingMinutes = HopOneRemainingMinutes + 1;
-}
-
-function subtract5Minutes()
-{
-    hop1Label.innerText = HopOneRemainingMinutes + " min : " + HopOneRemainingSeconds + " sec";
-    OneRemainingMinutes = HopOneRemainingMinutes - 5;
-}
-
-function subtract1Minute()
-{
-    hop1Label.innerText = HopOneRemainingMinutes + " min : " + HopOneRemainingSeconds + " sec";
-    HopOneRemainingMinutes = HopOneRemainingMinutes - 1;
 }
 
 function startHopOneTimer()
@@ -68,11 +51,8 @@ function resetHopOneTimer()
 function alarmHopOne()
 {
     console.log("Hop 1 timer done");
-
-    var audio = new Audio('./audio/timerAlarm.mp3');
     audio.play();
-
-    setTimeout(function(){ alert("Hop 1 Timer has Finshed!"); }, 1);
+    setTimeout(function(){ alert("Hop 1 Timer has Finshed!"); }, 3);
 }
 
 function everythingIsLoaded()
