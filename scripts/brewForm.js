b5d0f9556ec482503134a5713e753c9066a2e6a8
Miller <jordan.miller@hrblock.com>
2019-02-27 15:40:39 -0600

    updated brewForm html and js

--- scripts/brewForm.js
+++ scripts/brewForm.js
@@ -15,3 +15,17 @@ function writeToFile(firstName, lastName){
   
     document.body.removeChild(pom);
 }
+
+function emailResults(firstName, lastName, email){
+    var line1 = "First Name: " + firstName + "%0D%0A";
+    var line2 = "Last Name: " + lastName + "%0D%0A";
+    var line3 = "Email Used: " + email + "%0D%0A";
+    var body = line1 + line2 + line3;
+
+    var subject = "This is the Subject!!!";
+
+    var myString = 'mailto:' + email + '?subject=' + subject + '&body=' + body;
+    console.log("myString: ", myString);
+
+    window.open(myString);
+}
