798d58e951e9deb7629364e411042bf12e737a80
Miller <jordan.miller@hrblock.com>
2019-02-27 12:11:34 -0600

    brewForm js Functionallity

--- scripts/brewForm.js
+++ scripts/brewForm.js
@@ -1,7 +1,9 @@
 function writeToFile(firstName, lastName){
     var pom = document.createElement('a');
+
     pom.setAttribute('href', 'data:text/plain;charset=utf-8,' + 
     encodeURIComponent(firstName) + 
+    "\r\n" +
     encodeURIComponent(lastName));
 
     pom.setAttribute('download', 'brewFormTest.txt');
