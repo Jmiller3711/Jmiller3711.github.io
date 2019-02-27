8abddda96c0d08a4c230f5466213c622f1412c03
Miller <jordan.miller@hrblock.com>
2019-02-27 11:53:52 -0600

    created brewForm.html and brewForm.js

---
+++ scripts/brewForm.js
@@ -0,0 +1,15 @@
+function writeToFile(firstName, lastName){
+    var pom = document.createElement('a');
+    pom.setAttribute('href', 'data:text/plain;charset=utf-8,' + 
+    encodeURIComponent(firstName) + 
+    encodeURIComponent(lastName));
+
+    pom.setAttribute('download', 'brewFormTest.txt');
+  
+    pom.style.display = 'none';
+    document.body.appendChild(pom);
+  
+    pom.click();
+  
+    document.body.removeChild(pom);
+}
