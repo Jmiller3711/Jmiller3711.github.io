020ae3fa6efe2506970b86d55e7f83fafc8c58bc
Miller <jordan.miller@hrblock.com>
2019-03-01 08:44:55 -0600

    email functionallity added to brewForm

--- scripts/brewForm.js
+++ scripts/brewForm.js
@@ -16,10 +16,39 @@ function writeToFile(firstName, lastName){
     document.body.removeChild(pom);
 }
 
-function emailResults(firstName, lastName, email){
+function emailResults(totalGrainWeight, strikeWaterVolume, targetMashTemp, spargeWaterTemp, initialMashTemp, finalMashTemp, preBoilSpecificGravity, preBoilVolume, postBoilVolume, originalSpecificGravity, firstName, lastName, email){
+
     var line1 = "Hello " + firstName + " " + lastName + "," + "%0D%0A" + "%0D%0A";
-    var line2 = "Thank you for using my brew form. Below are your results:" + "%0D%0A";
-    var body = line1 + line2;
+    var line2 = "Thank you for using my brew form. Below are your results:" + "%0D%0A" + "%0D%0A";
+
+    var line3 = "Pre-Brew Calculations:" + "%0D%0A";
+    var line4 = "Total Grain Weight = " + totalGrainWeight + " lbs" + "%0D%0A";
+    var line5 = "Strike Water Volume = " + strikeWaterVolume + " gal" + "%0D%0A";
+    var line6 = "Target Mash Temp = " + targetMashTemp + " F" + "%0D%0A";
+    var line7 = "Sparge Water Temp = " + spargeWaterTemp + " F" + "%0D%0A" + "%0D%0A";
+
+    var line8 = "Mash Calculations:" + "%0D%0A";
+    var line9 = "Initial Mash Temperature = " + initialMashTemp + " F" + "%0D%0A";
+    var line10 = "Final Mash Temperature = " + finalMashTemp + " F" + "%0D%0A" + "%0D%0A";
+
+    var line11 = "Boil Calculations:" + "%0D%0A";
+    var line12 = "Pre-Boil SG  = " + preBoilSpecificGravity + "%0D%0A";
+    var line13 = "Pre-Boil V  = " + preBoilVolume + " gal" + "%0D%0A";
+    var line14 = "Post-Boil V  = " + postBoilVolume + " gal" + "%0D%0A" + "%0D%0A";
+
+    var line15 = "Post Boil Calculations:" + "%0D%0A";
+    var line16 = "Post-Boil Original SG  = " + originalSpecificGravity + "%0D%0A" + "%0D%0A";
+
+    var line17 = "JPMillerEngineering.com";
+
+    var greeting = line1 + line2;
+    var preBrew = line3 + line4 + line5 + line6 + line7;
+    var mash = line8 + line9 + line10;
+    var boil = line11 + line12 + line13 + line14;
+    var postBoil = line15 + line16;
+    var postScript = line17;
+
+    var body = greeting + preBrew + mash + boil + postBoil + postScript;
 
     var subject = "Brew Form Created by JPMillerEngineering.com";
 
